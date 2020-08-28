5c033e7524ec9761fc5e484fc4e46d50a5b67c84
yoda_of_codas \n zen_of_python <69405635+yoda-of-codas@users.noreply.github.com>
2020-08-28 10:06:11 +0100

    Main Matter

---
+++ number theory 6.py
@@ -0,0 +1,14 @@
+import math
+
+ans = []
+for i in range(1000,10000):
+   num = str(i)
+   #check to ensure the number format aabb is met
+   if num[0] == num[1] and num[2] == num[3]:
+      print('testing',i)
+      #check to see make sure the number is a perfect square.
+      #the normal root will be equal to the root rounded to any number in this case 5
+      if math.sqrt(i)==round(math.sqrt(i),5):
+         ans.append(i)
+print('The number aabb which is a perfect square is' ,ans)
+      
